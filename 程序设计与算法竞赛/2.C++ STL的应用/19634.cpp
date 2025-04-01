943866163af6603e13597d939fa0bd76f9958935
accelerator115 <lwh0721@outlook.com>
2025-04-01 17:39:26 +0800

    完成2.C++ STL的应用

---
+++ 程序设计与算法竞赛/2.C++ STL的应用/19634.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+        
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    int m;
+    cin >> m;
+    
+    for (int i = 0; i < m; i++) {
+        int num;
+        cin >> num;
+        
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+
+        double median;
+        if ((maxHeap.size() + minHeap.size()) % 2 == 1) {
+            median = maxHeap.top();
+        } else {
+            median = (maxHeap.top() + minHeap.top()) / 2.0;
+        }
+        
+        if (median == static_cast<int>(median)) {
+            cout << static_cast<int>(median) << endl;
+        } else {
+            cout << median << endl;
+        }
+    }
+    
+    return 0;
+}
