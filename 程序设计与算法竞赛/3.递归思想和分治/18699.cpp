8fb6e2b38fa3f1d34a4cd4939be48f81e510027e
accelerator115 <lwh0721@outlook.com>
2025-05-05 12:43:51 +0800

    添加注释

--- 程序设计与算法竞赛/3.递归思想和分治/18699.cpp
+++ 程序设计与算法竞赛/3.递归思想和分治/18699.cpp
@@ -1,34 +1,64 @@
 #include <iostream>
 using namespace std;
+
+// n: 待排序数组的元素个数
 int n;
+// data: 存储输入数据的数组
 int data[1001];
+// temp: 归并排序过程中用于临时存储的辅助数组
 int temp[1001];
+
+
 void merge_sort(int *A, int x, int y, int *T)
 {
+    // 如果区间长度大于1，才需要排序
     if (y - x > 1)
     {
+        // 计算中间点，将区间分成两部分
         int m = (x + y) / 2;
+        
+        // 递归排序左半部分 [x, m)
         merge_sort(A, x, m, T);
+        // 递归排序右半部分 [m, y)
         merge_sort(A, m, y, T);
+        
+        // 合并两个已排序的子区间
+        // p指向左半部分的起始位置，q指向右半部分的起始位置
+        // i是临时数组的当前填充位置
         int p = x, q = m, i = x;
+        
+        // 当左半部分或右半部分还有元素时，继续合并
         while (p < m || q < y)
         {
+            // 如果右半部分已经没有元素，或者左半部分的当前元素小于等于右半部分的当前元素
+            // 则将左半部分的当前元素放入临时数组
             if (q >= y || (p < m && A[p] <= A[q]))
                 T[i++] = A[p++];
+            // 否则将右半部分的当前元素放入临时数组
             else
                 T[i++] = A[q++];
         }
+        
+        // 将临时数组中的已排序元素复制回原数组
         for (i = x; i < y; i++)
             A[i] = T[i];
     }
+    // 对于长度为1或0的区间，不需要进行任何操作（已经是有序的）
 }
 
 int main()
 {
+    // 读取数组大小
     cin >> n;
+    
+    // 读取n个整数到data数组
     for (int i = 0; i < n; i++)
         cin >> data[i];
+    
+    // 调用归并排序函数对整个data数组进行排序
     merge_sort(data, 0, n, temp);
+    
+    // 按顺序输出排序后的数组元素
     for (int i = 0; i < n; i++)
         cout << data[i] << endl;
 }
--- 程序设计与算法竞赛/4.枚举的技巧/18443.cpp
+++ 程序设计与算法竞赛/4.枚举的技巧/18443.cpp
@@ -3,51 +3,76 @@
 #include <algorithm>
 
 using namespace std;
-typedef long long ll;
+typedef long long ll; // 定义长整型别名
 
+/**
+ * 检查两个数字a和b是否满足题目要求的数字不重复条件
+ * 
+ * @param a 第一个数(被除数)
+ * @param b 第二个数(除数)
+ * @return 如果a和b的所有非零数字都不重复，返回true；否则返回false
+ */
 bool check(int a, int b)
 {
-    int bl[15]={0};
+    int bl[15]={0}; // 标记数组，记录每个数字出现的次数
+    
+    // 处理第一个数a的每一位
     for (int i = 0; i <= 5; i++)
     {
-        int lmt = a%10;
-        if(bl[lmt]>0&&lmt!=0)
+        int lmt = a%10; // 获取a的最低位
+        if(bl[lmt]>0 && lmt!=0) // 如果该数字(非0)已经出现过，则返回false
             return false;
-        bl[lmt]++;
-        a/= 10;
+        bl[lmt]++; // 标记该数字已出现
+        a/= 10;    // 去除最低位
     }
+    
+    // 处理第二个数b的每一位
     for(int i = 0; i <= 5; i++)
     {
-        int lmt = b%10;
-        if(bl[lmt]>0&&lmt!=0)
+        int lmt = b%10; // 获取b的最低位
+        if(bl[lmt]>0 && lmt!=0) // 如果该数字(非0)已经出现过，则返回false
             return false;
-        bl[lmt]++;
-        b/= 10;
+        bl[lmt]++; // 标记该数字已出现
+        b/= 10;    // 去除最低位
     }
-    return true;
+    
+    return true; // 所有非零数字都不重复，返回true
 }
 
+/**
+ * 解决单个测试用例
+ * 寻找满足条件的除法表达式：o/i=n，且o和i的非零数字不重复
+ * 
+ * @param n 指定的除法结果
+ */
 void solve(int n)
 {
-    for(int i=1;i<=98765;i++)
+    // 枚举可能的除数i
+    for(int i=1; i<=98765; i++)
     {
-        int o=n*i;
-        if(o>99999)
+        int o = n*i; // 计算对应的被除数
+        
+        if(o>99999) // 如果被除数超过5位数，后续的i只会更大，直接结束
             break;
-        if(check(o,i))
-          printf("%05d/%05d=%d\n",o,i,n);
+            
+        // 检查o和i是否满足数字不重复的条件
+        if(check(o, i))
+          printf("%05d/%05d=%d\n", o, i, n); // 按格式输出结果
     }
 }
 
 int main()
 {
     int n;
-    scanf("%d",&n);
-    while(n!=0)
+    scanf("%d", &n);
+    
+    // 处理多个测试用例，直到输入0结束
+    while(n != 0)
     {
-        solve(n);
-        printf("\n");
-        scanf("%d",&n);
+        solve(n);     // 解决当前测试用例
+        printf("\n"); // 用例之间输出空行
+        scanf("%d", &n); // 读取下一个测试用例
     }
+    
     return 0;
 }
